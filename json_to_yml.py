3358f7a8783a9bebd95116c0768f81e743669516
DaviesH79 <daviesh679@gmail.com>
2021-03-22 16:00:36 -0400

    cleaning up, adding comments

--- json_to_yml.py
+++ json_to_yml.py
@@ -1,16 +1,13 @@
-import io
 import yaml
 import json
 from pprint import pprint
 
-#file = open("launchdata.txt", "r")
-#print(file.read())
-
-with io.open('launchdata.txt') as json_file:
+# open launchdata file and conver the json to a dictionary
+with open('launchdata.txt') as json_file:
 	data = json.load(json_file)
 	dataClean = data['data']
-	print(type(dataClean))
 
+# using the yaml library, write the dictionary to a yaml file
 with open('launchdata.yaml', 'w') as file:
 	launchData = yaml.safe_dump(dataClean, file, encoding='utf-8', allow_unicode=True)
 
